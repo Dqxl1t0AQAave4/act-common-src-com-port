bf0970a1d0e59a173471360348e14ddcb10ba6b5
kalaider <a.kalaider@yandex.ru>
2017-06-18 18:03:38 +0300

    /com-port-api byte_buffer documented

--- com-port-api/include/byte_buffer.h
+++ com-port-api/include/byte_buffer.h
@@ -4,6 +4,8 @@
 
 /*
  * Inspired by the `java.nio.ByteBuffer`.
+ * 
+ * See it and usage examples for more details.
  */
 
 class byte_buffer
@@ -11,7 +13,7 @@ class byte_buffer
 
 private:
 
-    std::vector<char> _data0;
+    std::vector<char> _data0; // to simplify implementation... ugly, but...
     std::vector<char> _data;
 
     std::size_t _position;
@@ -24,6 +26,15 @@ private:
 
 public:
 
+    /**
+     *	Creates new byte buffer with the following parameters:
+     *	
+     *	```
+     *	position = 0
+     *	limit    = initial
+     *	capacity = initial
+     *	```
+     */
     byte_buffer(std::size_t initial = 0)
         : _position(0)
         , _limit(initial)
@@ -32,79 +43,161 @@ public:
     {
     }
 
+    /**
+     *	Returns a pointer to the backing byte array
+     *	with `capacity` elements.
+     */
     char * buffer()
     {
         return this->_data.data();
     }
 
+    /**
+     *	Returns a pointer to the `position`-th element
+     *	of the backing byte array. The resulting array
+     *	will then have `capacity - position` size.
+     *	
+     *	Equivalent of `buffer() + position()`.
+     */
     char * data()
     {
         return buffer() + position();
     }
 
+    /**
+     *	Performs the following:
+     *	
+     *	```
+     *	limit    = position
+     *	position = 0
+     *	```
+     */
     byte_buffer & flip()
     {
         limit(position()).position(0);
         return *this;
     }
-
+    
+    /**
+     *	Returns the current position.
+     */
     std::size_t position() const
     {
         return this->_position;
     }
 
+    /**
+     *	Sets the current position.
+     *	
+     *	The function does not perform any assertions.
+     */
     byte_buffer & position(std::size_t new_position)
     {
         this->_position = new_position;
         return *this;
     }
 
+    /**
+     *	Increments the current position.
+     *	
+     *	The function does not perform any assertions.
+     */
     byte_buffer & increase_position(std::size_t increment)
     {
         position(position() + increment);
         return *this;
     }
 
+    /**
+     *	Returns the current capacity of the buffer.
+     */
     std::size_t capacity() const
     {
         return this->_data.size();
     }
 
+    /**
+     *	Sets the new capacity of the buffer.
+     *	
+     *	The function does not perform any assertions.
+     */
     byte_buffer & capacity(std::size_t new_capacity)
     {
         this->_data.resize(new_capacity);
         this->_data0.resize(new_capacity);
         return *this;
     }
-
+    
+    /**
+     *	Returns the amount of free space in the buffer.
+     *	
+     *	Equivalent of `limit() - position()`.
+     */
     std::size_t remaining() const
     {
         return limit() - position();
     }
-
+        
+    /**
+     *	Returns the current limit position.
+     */
     std::size_t limit() const
     {
         return this->_limit;
     }
-
+        
+    /**
+     *	Sets the current limit position.
+     *
+     *	The function does not perform any assertions.
+     */
     byte_buffer & limit(std::size_t new_limit)
     {
         this->_limit = new_limit;
         return *this;
     }
 
+    /**
+     *	Sets the buffer position to 0.
+     */
     byte_buffer & clear()
     {
         position(0);
         return *this;
     }
 
+    /**
+     *	Sets the buffer position to 0
+     *	and limit co capacity.
+     *	
+     *	```
+     *	position = 0
+     *	limit    = capacity
+     *	```
+     */
     byte_buffer & reset()
     {
         position(0).limit(capacity());
         return *this;
     }
 
+    /**
+     *	Compacts this buffer.
+     *	
+     *	Moves all the unprocessed data (between `position`
+     *	(inclusive) and `limit` (exclusive)) to the begin
+     *	of the buffer.
+     *	
+     *	Sets the current `position` to `remains` and `limit`
+     *	to `capacity`:
+     *	
+     *	```
+     *	data[position, limit] -> data[0, position]
+     *	
+     *	position = remains
+     *	limit = capacity
+     *	```
+     */
     byte_buffer & compact()
     {
         std::size_t remains = remaining();
@@ -119,6 +212,23 @@ public:
         return *this;
     }
 
+    /**
+     *  Inserts up to `size` bytes to this
+     *  buffer and increases its `position` to the
+     *  number of bytes actually inserted.
+     *  
+     *	Returns the number of unprocessed bytes
+     *	remaining in the `in` array.
+     *	
+     *	```
+     *	n = min(remaining, size);
+     *	
+     *	data[0, n] <- in[0, n]
+     *	position += n
+     *	
+     *	return (size - n)
+     *	```
+     */
     std::size_t put(const char *in, std::size_t size)
     {
         std::size_t new_position = position() + size;
@@ -143,7 +253,24 @@ public:
             return 0;
         }
     }
-
+    
+    /**
+     *  Takes up to `size` bytes from this
+     *  buffer and increases its `position` to the
+     *  number of bytes actually taken.
+     *  
+     *	Returns the number of unprocessed bytes
+     *	remaining in the `out` array.
+     *	
+     *	```
+     *	n = min(remaining, size);
+     *	
+     *	data[0, n] -> out[0, n]
+     *	position += n
+     *	
+     *	return (size - n)
+     *	```
+     */
     std::size_t get(char *out, std::size_t size)
     {
         std::size_t new_position = this->_position + size;
@@ -168,7 +295,17 @@ public:
             return 0;
         }
     }
-
+        
+    /**
+     *  Takes up to one byte from this buffer
+     *  and increases its `position` to the
+     *  number of bytes actually taken.
+     *  
+     *	Returns `true` if byte actually taken,
+     *	`false` otherwise.
+     *	
+     *	Equivalent of `get(&byte, 1) == 0`.
+     */
     bool get(char & byte)
     {
         return get(&byte, 1) == 0;
