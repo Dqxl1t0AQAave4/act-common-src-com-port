cf815fbfa717ade36fd11b0a159d9304b22ff730
kalaider <a.kalaider@yandex.ru>
2017-06-18 18:47:50 +0300

    /com-port-api com_port_options introduced
    com_port_options structure introduced which allows to specify some of
    the com port options (baud rate, frame format).

--- com-port-api/include/com-port.h
+++ com-port-api/include/com-port.h
@@ -2,6 +2,45 @@
 
 #include <afxwin.h>
 
+#include <logger.h>
+
+struct com_port_options
+{
+    com_port_options(CString name, DCB dcb)
+        : name(name)
+        , use_dcb(true)
+        , dcb(dcb)
+    {
+    }
+
+    com_port_options(CString name,
+                     size_t baudrate,
+                     size_t byte_size,
+                     bool   use_parity,
+                     size_t parity,
+                     size_t stop_bits)
+        : name(name)
+        , use_dcb(false)
+        , baudrate(baudrate)
+        , byte_size(byte_size)
+        , use_parity(use_parity)
+        , parity(parity)
+        , stop_bits(stop_bits)
+    {
+    }
+
+    CString name;
+
+    size_t  baudrate;
+    size_t  byte_size;
+    bool    use_parity;
+    size_t  parity;
+    size_t  stop_bits;
+
+    bool    use_dcb;
+    DCB     dcb;
+};
+
 class com_port
 {
 
@@ -57,11 +96,11 @@ public:
         return open();
     }
 
-    bool open(CString name)
+    bool open(com_port_options options)
     {
-        if (open() && (comm_name == name))
+        if (open() && (comm_name == options.name))
         {
-            logger::log(_T("port [%s] already opened"), name);
+            logger::log(_T("port [%s] already opened"), options.name);
             return true;
         }
         if (open())
@@ -70,11 +109,11 @@ public:
             {
                 return false;
             }
-            return open0(name);
+            return open0(options);
         }
         else
         {
-            return open0(name);
+            return open0(options);
         }
     }
 
@@ -137,11 +176,11 @@ public:
 
 private:
 
-    bool open0(CString name)
+    bool open0(com_port_options options)
     {
-        comm_name = name;
+        comm_name = options.name;
         comm = CreateFile(
-            name,
+            options.name,
             GENERIC_READ | GENERIC_WRITE,
             0,
             NULL,
@@ -154,11 +193,11 @@ private:
             DWORD errorMessageID = GetLastError();
             if (errorMessageID == ERROR_FILE_NOT_FOUND)
             {
-                logger::log(_T("serial port [%s] does not exist"), name);
+                logger::log(_T("serial port [%s] does not exist"), options.name);
             }
             else
             {
-                logger::log(_T("error occurred while opening [%s] port"), name);
+                logger::log(_T("error occurred while opening [%s] port"), options.name);
             }
             logger::log_system_error(errorMessageID);
             return false;
@@ -179,45 +218,52 @@ private:
             logger::log_system_error(GetLastError());
             CloseHandle(comm);
             comm = INVALID_HANDLE_VALUE;
-            logger::log(_T("cannot setup port [%s] timeouts"), name);
+            logger::log(_T("cannot setup port [%s] timeouts"), options.name);
             return false;
         }
 
         DCB ComDCM;
 
-        memset(&ComDCM, 0, sizeof(ComDCM));
-        ComDCM.DCBlength = sizeof(DCB);
-        GetCommState(comm, &ComDCM);
-        ComDCM.BaudRate = DWORD(4800);
-        ComDCM.ByteSize = 8;
-        ComDCM.Parity = ODDPARITY;
-        ComDCM.StopBits = ONESTOPBIT;
-        ComDCM.fAbortOnError = TRUE;
-        ComDCM.fDtrControl = DTR_CONTROL_DISABLE;
-        ComDCM.fRtsControl = RTS_CONTROL_DISABLE;
-        ComDCM.fBinary = TRUE;
-        ComDCM.fParity = TRUE;
-        ComDCM.fInX = FALSE;
-        ComDCM.fOutX = FALSE;
-        ComDCM.XonChar = 0;
-        ComDCM.XoffChar = (unsigned char) 0xFF;
-        ComDCM.fErrorChar = FALSE;
-        ComDCM.fNull = FALSE;
-        ComDCM.fOutxCtsFlow = FALSE;
-        ComDCM.fOutxDsrFlow = FALSE;
-        ComDCM.XonLim = 128;
-        ComDCM.XoffLim = 128;
+        if (options.use_dcb)
+        {
+            ComDCM = options.dcb;
+        }
+        else
+        {
+            memset(&ComDCM, 0, sizeof(ComDCM));
+            ComDCM.DCBlength = sizeof(DCB);
+            GetCommState(comm, &ComDCM);
+            ComDCM.BaudRate = DWORD(options.baudrate);
+            ComDCM.ByteSize = options.byte_size;
+            ComDCM.Parity = options.parity;
+            ComDCM.StopBits = options.stop_bits;
+            ComDCM.fAbortOnError = TRUE;
+            ComDCM.fDtrControl = DTR_CONTROL_DISABLE;
+            ComDCM.fRtsControl = RTS_CONTROL_DISABLE;
+            ComDCM.fBinary = TRUE;
+            ComDCM.fParity = options.use_parity;
+            ComDCM.fInX = FALSE;
+            ComDCM.fOutX = FALSE;
+            ComDCM.XonChar = 0;
+            ComDCM.XoffChar = (unsigned char) 0xFF;
+            ComDCM.fErrorChar = FALSE;
+            ComDCM.fNull = FALSE;
+            ComDCM.fOutxCtsFlow = FALSE;
+            ComDCM.fOutxDsrFlow = FALSE;
+            ComDCM.XonLim = 128;
+            ComDCM.XoffLim = 128;
+        }
 
         if (!SetCommState(comm, &ComDCM))
         {
             logger::log_system_error(GetLastError());
             CloseHandle(comm);
             comm = INVALID_HANDLE_VALUE;
-            logger::log(_T("cannot setup port [%s] configuration"), name);
+            logger::log(_T("cannot setup port [%s] configuration"), options.name);
             return false;
         }
 
-        logger::log(_T("successfully connected to [%s] port"), name);
+        logger::log(_T("successfully connected to [%s] port"), options.name);
 
         return true;
     }
