ae03aa588b16ac6402a5d0b319fdf2e32de93612
kalaider <a.kalaider@yandex.ru>
2017-06-28 02:03:28 +0300

    /com-port-api state_diagram and state_machine added
    New classes introduced.
    
    State diagram is responsible for calculating an appropriate state basing
    on the current state, flags, etc.
    
    State machine provides a generic mechanism of state transitions. It
    actually interacts with some variables, not just performs calculations.
    
    Two such entities are separately introduced to divide calculation logic
    and state transition logic to allow switching between different diagrams
    and machines in a simple way.

--- com-port-api/include/channel.h
+++ com-port-api/include/channel.h
@@ -103,13 +103,35 @@ namespace com_port_api
             using predicate_t = std::function < bool (const state_t &) > ;
 
         };
+        
 
-        using error_handler_t   = std::function < void (const error::channel_error &) > ;
-        using success_handler_t = std::function < void () > ;
-        using result_handler_t  = std::function < void (byte_buffer &) > ;
+        struct ops
+        {
+
+            static enum : constant_t
+            {
+                open  = (1 << 0),
+                read  = (1 << 1),
+                write = (1 << 2),
+                close = (1 << 3)
+            };
+
+            static enum class result_type
+            {
+                result_success, result_failure, guarantee
+            };
+        };
+        
+
+        enum class guarantee_t
+        {
+            release = std::memory_order_release,
+            acquire = std::memory_order_acquire,
+            acq_rel = std::memory_order_acq_rel
+        };
 
 
-        class channel_base
+        class state_diagram
         {
 
 
@@ -118,19 +140,152 @@ namespace com_port_api
 
             /**
              *	std::get<0> -> success/failure
+             *	std::get<1> -> calculated state
+             *	std::get<2> -> calculated guarantee
+             */
+            using result_t = std::tuple < bool, state_t, guarantee_t > ;
+
+
+        public:
+
+
+            virtual ~state_diagram()
+            {
+            }
+
+
+            virtual result_t lock_op  (constant_t op,
+                                       state_t    started_with,
+                                       flags_t    flags) const = 0;
+
+            virtual result_t unlock_op(constant_t       op,
+                                       state_t          started_with,
+                                       state_t          locked_with,
+                                       flags_t          flags,
+                                       ops::result_type op_result) const = 0;
+
+        };
+
+
+        class state_machine
+        {
+
+
+        public:
+
+            
+            /**
+             *	std::get<0> -> success/failure
+             *	std::get<1> -> state on a moment of invocation
+             *	std::get<2> -> new state set
+             */
+            using result_t = std::tuple < bool, state_t, state_t > ;
+
+
+        public:
+
+            virtual ~state_machine()
+            {
+            }
+            
+
+            virtual state_t state() const = 0;
+            
+            virtual result_t set_state(state_t     expected,
+                                       state_t     desired,
+                                       guarantee_t guarantee = guarantee_t::acq_rel) = 0;
+
+
+            virtual flags_t flags() const = 0;
+            
+            virtual void set_flags(flags_t desired) = 0;
+
+
+            virtual result_t lock_op  (state_diagram &d,
+                                       constant_t    op) = 0;
+
+            virtual result_t unlock_op(state_diagram    &d,
+                                       constant_t       op,
+                                       state_t          locked_with,
+                                       ops::result_type op_result) = 0;
+
+
+            virtual void provide_guarantee(guarantee_t guarantee = guarantee_t::acq_rel)
+            {
+                result_t result { false, state(), states::none };
+                do 
+                {
+                    result = set_state(std::get<1>(result), std::get<1>(result), guarantee);
+                } while (!std::get<0>(result));
+            }
+
+            virtual bool wait_unconditionally(states::predicate_t predicate) const = 0;
+
+        };
+
+
+        using error_handler_t   = std::function < void (const error::channel_error &) > ;
+        using success_handler_t = std::function < void () > ;
+        using result_handler_t  = std::function < void (byte_buffer &) > ;
+
+
+        template <class SD, class SM,
+                  typename = std::enable_if_t<std::is_base_of<typename state_diagram, SD>::value
+                                           && std::is_base_of<typename state_machine, SM>::value>>
+        class channel_base
+        {
+
+
+        public:
+
+
+            using state_diagram_t = SD;
+            using state_machine_t = SM;
+            
+            /**
+             *	std::get<0> -> success/failure
              *	std::get<1> -> state on a moment of invocation
              *	std::get<2> -> new state set
              */
             using result_t = std::tuple <bool, state_t, state_t> ;
 
 
+        protected:
+
+
+            state_diagram_t _diagram;
+            state_machine_t _machine;
+
+
         public:
 
+
+            channel_base(const state_diagram_t &diagram,
+                         const state_machine_t &machine)
+                         : _diagram(diagram), _machine(machine)
+            {
+            }
+
+            channel_base()
+            {
+            }
 
             virtual ~channel_base()
             {
             }
 
+
+        public:
+
+            const state_diagram_t & get_state_diagram()
+            {
+                return _diagram;
+            }
+
+            const state_machine_t & get_state_machine()
+            {
+                return _machine;
+            }
 
         public:
 
